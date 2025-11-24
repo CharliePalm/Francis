9bddb15796f7c26e4d5af3da77d29185ae5ffb78
Charlie Palm <cwpalm@wisc.edu>
2025-11-24 16:27:37 -0600

    codify | integrate to use tracker

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -11,40 +11,37 @@ export function getLogicChildren(block: string) {
   if (!block.startsWith('if(') || !block.endsWith('}')) {
     throw new Error('getLogicChildren called with non logic block - ' + block);
   }
-  let index = block.indexOf('(') + 1; // start after if( or elseIf call
-  let depth = 1; // because we start right after the if
-  const depthChars = ['(', '{'];
-  const deDepthChars = [')', '}'];
-  let bottomIdx = index;
+  const tracker = new DepthTracker(block);
+  while (tracker.depth === 0) {
+    tracker.inc();
+  }
+  // + 1 here to move past the ( char
+  let bottomIdx = tracker.index + 1;
   const blocks = new Array(3);
   let blockIdx = 0;
-  while (index < block.length) {
-    if (depthChars.includes(block[index])) {
-      depth++;
-      if (depth === 1 && block[index] === '{') {
-        bottomIdx = index + 1;
-      }
-    } else if (deDepthChars.includes(block[index])) depth--;
+  while (tracker.inc()) {
+    if (tracker.depth === 1 && tracker.currChar === '{') {
+      bottomIdx = tracker.index + 1;
+    }
     // looking for what's in the if(...) block
     if (blockIdx === 0) {
-      if (depth === 0 && block[index] === ')') {
-        blocks[blockIdx++] = block.substring(bottomIdx, index);
+      if (tracker.depth === 0 && tracker.currChar === ')') {
+        blocks[blockIdx++] = block.substring(bottomIdx, tracker.index);
       }
-    } else if (depth === 0 && block[index] === '}') {
+    } else if (tracker.depth === 0 && tracker.currChar === '}') {
       // getting the if(){...} part of the block (true case)
-      blocks[blockIdx++] = block.substring(bottomIdx, index);
+      blocks[blockIdx++] = block.substring(bottomIdx, tracker.index);
       // getting the false case
-      while (
-        !deDepthChars.concat(depthChars).includes(block[++index]) &&
-        index < block.length
-      ) {}
+      const currDepth = tracker.depth;
+      while (currDepth === tracker.depth && tracker.inc()) {}
       // add 'if' if it was present (i.e. in an elseif block)
-      const prefix = block.substring(index - 2, index) === 'if' ? 'if(' : '';
-      blocks[blockIdx++] =
-        prefix + block.substring(index + 1, block.length - (prefix ? 0 : 1));
+      const prefix =
+        block.substring(tracker.index - 2, tracker.index) === 'if' ? 'if(' : '';
+      blocks[blockIdx] =
+        prefix +
+        block.substring(tracker.index + 1, block.length - (prefix ? 0 : 1));
       return blocks;
     }
-    index++;
   }
   throw new Error(
     `ran out of while loop while looking for logic children for block - ${block}, found children: ${blocks}`
