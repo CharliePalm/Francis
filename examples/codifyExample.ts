6dec6615af463c64535dd58ae9a430a09bd6a611
Charlie Palm <cwpalm@wisc.edu>
2026-02-05 11:37:18 -0600

    codify | organize

--- examples/codifyExample.ts
+++ examples/codifyExample.ts
@@ -1,28 +1,25 @@
-import { NotionFormulaCodifier } from './src/NotionFormulaCodifier';
+import { NotionFormulaCodifier } from '../src/NotionFormulaCodifier';
 const formula =
   'if(prop("Tags")=="PROF",("+"+format(ceil(toNumber(prop("ClassLevel").at(0).prop("Level"))/4)+1)),((prop("Amount")>=10?"+":"")+format(floor((prop("Amount")-10)/2))))';
 const result = new NotionFormulaCodifier(formula, {
   ['Amount']: {
     name: 'Amount',
     type: 'number',
-    rollup: {},
   },
   ['Tags']: {
     name: 'Tags',
     type: 'select',
-    number: {},
   },
   ['ClassLevel']: {
     name: 'ClassLevel',
     type: 'relation',
-    checkbox: {},
   },
 })
   .decompile()
   .then((res) => console.log(res));
 
-import { NotionFormulaGenerator } from './src/NotionFormulaGenerator';
-import * as Model from './src/model';
+import { NotionFormulaGenerator } from '../src/NotionFormulaGenerator';
+import * as Model from '../src/model';
 class MyFirstFormula extends NotionFormulaGenerator {
   public amount = new Model.Number('Amount');
   public tags = new Model.Select('Tags');
